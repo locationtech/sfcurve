ee518a2cde73e7d5d7a681c0e002893e6dc873c1
Anthony Fox <anthonyfox@ccri.com>
2016-02-09 16:52:48 -0500

    Fix resolution of Z2 dimensions to 31
    Signed-off-by: Jim Hughes <jnh5y@ccri.com>

--- zorder/src/test/scala/org/locationtech/sfcurve/zorder/ZCurve2DSpec.scala
+++ zorder/src/test/scala/org/locationtech/sfcurve/zorder/ZCurve2DSpec.scala
@@ -12,9 +12,9 @@ class ZCurve2DSpec extends FunSpec with Matchers {
 
     it("computes a covering set of ranges") {
       val sfc = SpaceFillingCurves("zorder", Map(ZOrderSFCProvider.RESOLUTION_PARAM -> Int.box(1024)))
-      val ranges = sfc.toRanges(-80.0, 35.0, -75.0, 40.0, ZCurve2D.hints(maxRecurse = 12))
+      val ranges = sfc.toRanges(-80.0, 35.0, -75.0, 40.0, ZCurve2D.hints(maxRecurse = 5))
 
-      ranges.length shouldBe 18
+      ranges.length shouldBe 19
       val (l, r, contains) = ranges.head.tuple
       l shouldBe 197616
       r shouldBe 197631

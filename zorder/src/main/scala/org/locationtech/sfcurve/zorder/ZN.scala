48cef67ea74c9ad44ac306bffb09dd7dec485bb0
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2016-07-18 10:37:07 -0400

    adding both copyrights

--- zorder/src/main/scala/org/locationtech/sfcurve/zorder/ZN.scala
+++ zorder/src/main/scala/org/locationtech/sfcurve/zorder/ZN.scala
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2013-2016 Commonwealth Computer Research, Inc.
+ * Copyright (c) 2015 Azavea.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Apache License, Version 2.0 which
  * accompanies this distribution and is available at
